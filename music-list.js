572ac3a8e2cc8350c0f33aa40e8df40befafc41f
EnSocietyy <142369638+EnSocietyy@users.noreply.github.com>
2023-09-28 09:14:36 +0700

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    {
+      name: "STAND HERE ALONE - HILANG HARAPAN",
+      artist: "STAND HERE ALONE",
+      img: "THREESIXTY",
+      src: "STAND HERE ALONE - HILANG HARAPAN"
+    },
+    {
+      name: "THREESIXTY - DEWI",
+      artist: "THREESIXTY",
+      img: "THREESIXTY",
+      src: "THREESIXTY - DEWI"
+    },
+    {
+      name: "CERITA MALAM - KARNA MEREKA",
+      artist: "THREESIXTY",
+      img: "THREESIXTY",
+      src: "CERITA MALAM - KARNA MEREKA"
+    },
+    {
+      name: "AYAH - KARNAMEREKA",
+      artist: "KARNA MEREKA",
+      img: "EMILIA - STAY ALIVE IKX REMIX",
+      src: "AYAH - KARNAMEREKA"
+    },
+    {
+      name: "THREESIXTY  - MENGHILANG DAN BERARTI",
+      artist: "THREESIXTY",
+      img: "THREESIXTY",
+      src: "THREESIXTY  - MENGHILANG DAN BERARTI"
+    },
+]
